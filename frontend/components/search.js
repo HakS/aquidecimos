b324bd2721adccd82308ab4c95dd6edfed0c4153
Juan Barba <juan.barba.o@gmail.com>
2022-03-07 18:34:39 -0500

    Tried to apply some memoized code on search component

--- frontend/components/search.js
+++ frontend/components/search.js
@@ -1,10 +1,43 @@
 import groq from 'groq';
 import Link from 'next/link';
-import { useCallback, useRef, useState, useEffect } from 'react';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 import { backend_RxJS } from '../client';
 import { useRouter } from 'next/router';
 
-let searchReq = null
+let searchSubscriber = null
+
+const fetchResults = async query => {
+  const q = groq`*[_type in ["meaning", "signified"] && signifier match $query] {
+    signifier,
+    _type
+  }`
+  if (searchSubscriber != null) {
+    searchSubscriber.unsubscribe()
+  }
+  return new Promise((res, rej) => {
+    searchSubscriber = backend_RxJS.fetch(q, { query: `${query}*` }).subscribe({
+      next: result => res(result),
+      error: err => rej(err)
+    })
+  })
+}
+
+const SearchResultsList = React.memo(({word, results}) => {
+  console.log(`CALLED! word: ${word}`)
+  return (
+    <ul className="absolute top-full w-full bg-white border border-slate-200 z-10 shadow-lg">
+      { results.map(result => (
+        <li key={ result.word }>
+          <Link href={result.link} passHref={true}>
+            <a className="p-3 block hover:bg-slate-100">{result.word}</a>
+          </Link>
+        </li>
+      )) }
+    </ul>
+  )
+}, (prev, next) => {
+  return prev.word === next.word
+})
 
 const Search = () => {
   const searchRef = useRef(null)
@@ -15,7 +48,6 @@ const Search = () => {
 
   useEffect(() => {
     document.addEventListener("keydown", escFunction, false);
-
     return () => {
       document.removeEventListener("keydown", escFunction, false);
     };
@@ -27,52 +59,40 @@ const Search = () => {
     }
   }, [router.asPath])
 
-  const escFunction = useCallback((event) => {
+  const escFunction = event => {
     if (event.key === "Escape") {
       searchRef.current.querySelector('input').blur()
       setActive(false)
     }
-  }, []);
-
-  const fetchResults = async query => {
-    const q = groq`*[_type in ["meaning", "signified"] && signifier match $query] {
-      signifier,
-      _type
-    }`
-    if (searchReq != null) {
-      searchReq.unsubscribe()
-    }
-    searchReq = backend_RxJS.fetch(q, { query: `${query}*` }).subscribe({
-      next: result => {
-        setResults(result.map(({_type, signifier}) => ({
-          word: signifier,
-          link: `/${ _type == 'meaning' ? 'expresion' : 'concepto' }/${ signifier }`
-        })))
-      }
-    })
   }
 
   const onChange = useCallback(event => {
-    const query = event.target.value;
-    setQuery(query)
+    const query = event.target.value
     if (query.length) {
-      fetchResults(query)
+      fetchResults(query).then(result => {
+        setResults(result.map( ({_type, signifier}) => ({
+          word: signifier,
+          link: `/${ _type == 'meaning' ? 'expresion' : 'concepto' }/${ signifier }`
+        })))
+        setQuery(query)
+      })
     } else {
       setResults([])
+      setQuery(query)
     }
-  }, [])
+  })
 
-  const onFocus = useCallback(() => {
+  const onFocus = () => {
     setActive(true)
     window.addEventListener('click', onClick)
-  }, [])
+  }
 
-  const onClick = useCallback(event => {
+  const onClick = event => {
     if (searchRef.current && !searchRef.current.contains(event.target)) {
       setActive(false)
       window.removeEventListener('click', onClick)
     }
-  }, [])
+  }
 
   return (
     <div className="w-full flex grow shrink-0 relative" ref={searchRef}>
@@ -83,15 +103,7 @@ const Search = () => {
       className="p-3 grow shrink-0"
       placeholder="Escribe cualquier palabra aquí..." />
       { active && results.length > 0 && (
-        <ul className="absolute top-full w-full bg-white border border-slate-200 z-10">
-          { results.map(result => (
-            <li key={ result.word }>
-              <Link href={result.link} passHref={true}>
-                <a className="p-3 block hover:bg-slate-100">{result.word}</a>
-              </Link>
-            </li>
-          )) }
-        </ul>
+        <SearchResultsList word={query} results={results} />
       )}
     </div>
   )

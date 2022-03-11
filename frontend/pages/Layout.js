8f32da18c7ff6bfb611fa6cb1596236a479bfbe1
Juan Barba <juan.barba.o@gmail.com>
2022-03-11 12:26:49 -0500

    Fixed gtag page view bug

--- frontend/pages/Layout.js
+++ frontend/pages/Layout.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { gtagPageView } from '../utils';
 
 export default (props) => {
   const router = useRouter()

f24c459aa54c80e8c9ade100f92316f0e4c5d441
Juan Barba <juan.barba.o@gmail.com>
2022-03-17 19:22:50 -0500

    - Added donation modal - Added links to donate via PayPal, Buy me a Coffee and Bitcoin

--- frontend/pages/Layout.js
+++ frontend/pages/Layout.js
@@ -2,13 +2,18 @@ import Head from 'next/head';
 import Search from '../components/search';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { gtagPageView } from '../utils';
 import Script from 'next/script';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import Modal from 'react-modal'
+import { SiBuymeacoffee } from 'react-icons/si';
+import { FaFacebook, FaInstagram, FaPaypal, FaBitcoin } from 'react-icons/fa';
+
+Modal.setAppElement('#__next')
 
 export default (props) => {
+  const [modal, setModal] = useState(false)
   const router = useRouter()
 
   useEffect(() => {
@@ -21,8 +26,34 @@ export default (props) => {
     }
   }, [router.events])
 
+  const handleDonationClick = () => {
+    setModal(true)
+  }
+
   return (
     <>
+      <Modal
+        isOpen={modal}
+        onRequestClose={() => setModal(false)}
+        contentLabel="Donar"
+      >
+        <h1 className="font-bold text-xl">Donar</h1>
+        <div className="grid gap-3 grid-cols-2 text-center">
+          <a href="https://www.buymeacoffee.com/aquidecimos" target="_blank">
+            <SiBuymeacoffee />
+            Buy me a coffee (Cómprame un café)
+          </a>
+          <a href="https://www.paypal.com/donate/?hosted_button_id=HVRPCFGRZD3WL" target="_blank">
+            <FaPaypal />
+            PayPal
+          </a>
+          <a href="https://www.blockonomics.co/pay-url/54dff52c5e5a4777" target="_blank">
+            <FaBitcoin />
+            Bitcoin
+          </a>
+        </div>
+      </Modal>
+
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
@@ -63,21 +94,29 @@ export default (props) => {
               { props.children }
             </main>
             <div className="w-full md:w-4/12 text-slate-400 text-sm text-center">
-              <aside>
-                <div className="flex gap-3 w-full justify-center mb-3">
-                  <a className="w-50 flex justify-center gap-2 items-center" href="https://www.facebook.com/aqui.decimos.diccionario" target="_blank">
-                    <FaFacebook className="text-lg" />
-                    Facebook
-                  </a>
-                  <a className="w-50 flex justify-center gap-2 items-center" href="https://www.instagram.com/aquidecimos.app/" target="_blank">
-                    <FaInstagram className="text-lg" />
-                    Instagram
-                  </a>
-                </div>
-              </aside>
-              <footer>
-                Aqui Decimos, { new Date().getFullYear() } todos los derechos reservados.
-              </footer>
+              <div className='sticky top-0'>
+                <aside>
+                  <div className="mb-5 flex gap-1 justify-center">
+                    <Link href='/nueva-palabra' passHref={true}>
+                      <a className="bg-blue-700 text-white py-3 px-4 inline-block uppercase font-bold hover:bg-blue-600 active:bg-blue-800">Nueva palabra</a>
+                    </Link>
+                    <a onClick={handleDonationClick} className="cursor-pointer bg-blue-700 text-white py-3 px-4 inline-block uppercase font-bold hover:bg-blue-600 active:bg-blue-800">Donar</a>
+                  </div>
+                  <div className="flex gap-3 w-full justify-center mb-3">
+                    <a className="w-50 flex justify-center gap-2 items-center hover:text-blue-600 active:text-blue-800" href="https://www.facebook.com/aqui.decimos.diccionario" target="_blank">
+                      <FaFacebook className="text-lg" />
+                      Facebook
+                    </a>
+                    <a className="w-50 flex justify-center gap-2 items-center hover:text-pink-600 active:text-pink-800" href="https://www.instagram.com/aquidecimos.app/" target="_blank">
+                      <FaInstagram className="text-lg" />
+                      Instagram
+                    </a>
+                  </div>
+                </aside>
+                <footer>
+                  Aqui Decimos, { new Date().getFullYear() } todos los derechos reservados.
+                </footer>
+              </div>
             </div>
           </div>
         </div>
